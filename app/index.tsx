0f94282190d01a5aa3416ac38817268d79286abe
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-12-18 20:53:06 +0000

    Update dependencies and configurations for Tailwind CSS and Nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,11 @@
 import {View, Text } from 'react-native';
-
-
+// Import your global CSS file
+import "../global.css";
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-    <Text>Home Screen</Text>
-   </View>
+      <Text>Home Screen</Text>
+    </View>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-      "/**/*.{js,jsx,ts,tsx}",
-      "./App.{js,jsx,ts,tsx}", /// Include the main App.js and App.tsx files 
-      "./app/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the app folder
-      "./components/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the components folder]
-      "./app/(tabs)/meditate.tsx",
-  ],
-  presets: [require("nativewind/preset")],
-  theme: {
+    content: [
+        "./App.{js,jsx,ts,tsx}",
+        "./app/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the app folder
+        "./components/**/*.{js,jsx,ts,tsx}", // Include all JS, JSX, TS, and TSX files in the components folder]
+        "./app/(tabs)/meditate.tsx",
+    ],
+    presets: [require("nativewind/preset")],
+    theme: {
       extend: {
-          fontFamily: {
-              rmono: ["Roboto-Mono", "sans-serif"],
-          },
+        fontFamily: {
+          rmono: ["Roboto-Mono", "sans-serif"],
+        },
       },
-  },
-  plugins: [],
-};
+    },
+    plugins: [],
+  };
