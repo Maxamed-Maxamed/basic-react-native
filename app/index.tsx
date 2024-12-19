76de896cb3952ccc9ade68538b53cad93bc673c6
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-12-19 17:39:30 +0000

    Enhance App component with LinearGradient and SafeAreaView; add expo-linear-gradient dependency

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,20 @@
-import { ImageBackground, StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, SafeAreaView, Text, View } from 'react-native'
 import React from 'react'
-import beach from '@/assets/meditation-images/beach.webp'
+import beachImg from '@/assets/meditation-images/beach.webp'
+import { LinearGradient } from 'expo-linear-gradient'
+
 const App = () => {
   return (
-    <View className='flex-1 items-center justify-center'>
-      <ImageBackground  source={beach} resizeMode="cover" className="flex-1"  > 
+    <View className='flex-1'>
+      <ImageBackground source={beachImg} resizeMode="cover" className="flex-1">
+        <LinearGradient className='flex-1' colors={['rgba(0,0,0,0.4)', 'rgba(0,0,0,0.8)']}>
+          <SafeAreaView className='text-center text-white font-bold text-4xl '>
+          <Text>App</Text>
 
-      <Text>App</Text>
-
+          </SafeAreaView>
+        </LinearGradient>
       </ImageBackground>
-     
     </View>
-    
   )
 }
 
