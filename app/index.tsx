e0b2596321994edb90c871f4e93c7fc88fbb5eee
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-12-19 17:16:04 +0000

    Refactor App component layout and add TypeScript declarations for additional media file types

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,15 @@
 import { ImageBackground, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-import { StatusBar } from 'expo-status-bar';
 import beach from '@/assets/meditation-images/beach.webp'
 const App = () => {
   return (
-    <View className='flex-1'>
-      <ImageBackground 
-        source={beach}
-        resizeMode="cover" 
-        className="flex-1" 
-      > 
+    <View className='flex-1 items-center justify-center'>
+      <ImageBackground  source={beach} resizeMode="cover" className="flex-1"  > 
 
+      <Text>App</Text>
 
       </ImageBackground>
-      <Text>App</Text>
-      <StatusBar style="auto" />
+     
     </View>
     
   )
--- images.d.ts
+++ images.d.ts
@@ -18,3 +18,99 @@ declare module '*.webp' {
   const value: import('react-native').ImageSourcePropType;
   export default value;
 }
+
+declare module '*.svg' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+
+declare module '*.gif' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.bmp' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.ico' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.icns' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+
+declare module '*.mp4' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+
+declare module '*.webm' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.mov' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.mp3' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.wav' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.aac' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.ogg' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.flac' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.m4a' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.wma' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.3gp' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.3g2' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.avi' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.flv' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.mkv' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.webm' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.wmv' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
+declare module '*.mov' {
+  const value: import('react-native').ImageSourcePropType;
+  export default value;
+}
